08e23e3776ff18254811d9d7affd6254850d0a62
Stanislav Sirychenko <sirichenkostas@gmail.com>
2025-09-18 12:45:38 +0300

    Fix typo: "DeffaultConnection" to "DefaultConnection".

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DeffaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDataServices();
 
